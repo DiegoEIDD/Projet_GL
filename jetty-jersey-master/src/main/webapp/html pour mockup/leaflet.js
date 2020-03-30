f30ebf5ad675507888b3d29932a800a50250fe68
guoyangming <61230003+guoyangming@users.noreply.github.com>
2020-03-31 06:45:32 +0800

    Add files via upload

---
+++ jetty-jersey-master/src/main/webapp/html pour mockup/leaflet.js
@@ -0,0 +1,16 @@
+
+
+$(function (){
+
+		var mymap = L.map('mapleflet1').setView([51.505, -0.09], 13);
+		L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    	maxZoom: 18,
+    	id: 'mapbox/streets-v11',
+    	tileSize: 512,
+    	zoomOffset: -1,
+    	accessToken: 'pk.eyJ1IjoibGVkb2RvaW5jb25udSIsImEiOiJjazg3OXNkZWUwZDdiM2VwcmQxbjI0cWczIn0.gsqi-Jp2FZ066BoQ_0IEJQ'
+		}).addTo(mymap);
+
+});
+
